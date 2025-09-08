a9be93c582a7f9e00a20a485b9a244721a6d582a
JornEirikBetten <jorneirikbetten1@gmail.com>
2025-09-08 16:53:56 +0200

    Updated learning rate and number of epochs (100)

--- data_augmentation.sh
+++ data_augmentation.sh
@@ -9,8 +9,10 @@
 #SBATCH -c 6          #  På g001 bør en berense seg til 96 / 16 cores per GPU
 #SBATCH --gres=gpu:1
 
-srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.1 eval_interval=20 dropout=True epochs=201 batch_size=16
-srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.1 eval_interval=20 dropout=True epochs=201 batch_size=32
-srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.1 eval_interval=20 dropout=True epochs=201 batch_size=64
-srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.1 eval_interval=20 dropout=True epochs=201 batch_size=128
-srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.1 eval_interval=20 dropout=True epochs=201 batch_size=256
+
+srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.02 eval_interval=10 dropout=True batch_size=512
+srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.02 eval_interval=10 dropout=True batch_size=256
+srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.02 eval_interval=10 dropout=True batch_size=128
+srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.02 eval_interval=10 dropout=True batch_size=64
+srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.02 eval_interval=10 dropout=True batch_size=32
+srun -n 1 python training_scheme_data_augmentation.py dataset=cifar10 model=resnet18 batch_norm=True lr=0.02 eval_interval=10 dropout=True batch_size=16
